6020d887efe9c733634cfa58437ab17cec08fdfe
Ssssssaraa <120736328+Ssssssaraa@users.noreply.github.com>
2022-12-16 16:31:10 +0000

    Had

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function pohniHadem(dolu, doprava) {
-  const had = document.querySelector(".had");
-  console.log("Had je na " + had.id);
+let had = [document.querySelector ("had")]
 
-  let radek = parseInt(had.id.split(":")[0]);
-  let sloupec = parseInt(had.id.split(":")[1]);
+function pohniHadem(dolu, doprava){ 
+  const hadiHlava = had [0]
+  console.log("Had je na " + hadiHlava.id);
+
+  let radek = parseInt(hadiHlava.id.split(":")[0]);
+  let sloupec = parseInt(hadiHlava.id.split(":")[1]);
   const idCil = radek + dolu + ":" + (sloupec + doprava);
   console.log("Had bude na " + idCil);
 
-  const cil = document.getElementById(idCil);
-  cil.classList.add("had");
-  had.classList.remove("had");
-}
+  const cilovePolicko = document.getElementById(idCil);
 
+  had.unshift (cilovePolicko);
+
+  cilovePolicko.classList.add("had")
+
+if (cilovePolicko.classList.contains("zradlo")) {
+  console.log ("had bude zrat");
+  cilovePolicko.classList.remove("zradlo")}
+else{ 
+  const polickokterePrestaneBythadem = had.pop();
+  polickokterePrestaneBythadem.classList.remove("had");
+  }
+}
 function pohyb(udalost) {
   if (udalost.which === 37) {
     console.log("Hade, jdi doleva pls");
